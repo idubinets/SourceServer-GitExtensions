0fb58bb5520314ed65b10718ba507dd47bd8b178
Ivan Dubynets <ivan@infostroy.com.ua>
2015-09-17 16:18:02 +0300

    fixed other small issue

--- RunAsAdmin-Install.bat
+++ RunAsAdmin-Install.bat
@@ -5,8 +5,8 @@ if not %ERRORLEVEL% == 0 echo "Perl not found in path.  It can be found in the m
 call git --version > NUL
 if not %ERRORLEVEL% == 0 echo "git not found in path." && exit /b 1
 ::Put the SourceServer files into their necessary location
-echo - Copying necessary files '%PROGRAMFILES%\Debugging Tools for Windows (x64)\srcsrv'...
-xcopy /y SourceServer\* "%PROGRAMFILES%\Debugging Tools for Windows (x64)\srcsrv" > NUL
+echo - Copying necessary files '%PROGRAMFILES%\Debugging Tools for Windows (x64)\srcsrv\'...
+xcopy /y SourceServer\* "%PROGRAMFILES%\Debugging Tools for Windows (x64)\srcsrv\" > NUL
 if not %ERRORLEVEL% == 0 echo Copy Failed && exit /b 1
 echo - Copying necessary files '%VS100COMNTOOLS%'...
 xcopy /y %~dp0\InSystemPath\* "%VS100COMNTOOLS%" > NUL
